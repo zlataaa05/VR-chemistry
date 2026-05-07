32ec0eb9df05874e36bc4725d65044ce42da06b8
Zlata <zlataacenko1610@gmail.com>
2026-05-08 02:26:13 +0300

    Взаимодействие с таблицей
    Обновлен способ взаимодействия с таблицей - добавлена указка с лучом. При наведении луча указки и нажатии триггера одновременно создается атом. Необходимо удалить возможность взаимодействия с таблицей при помощи руки

--- Scene/main.gd
+++ Scene/main.gd
@@ -4,10 +4,17 @@ extends Node3D
 @onready var table_area = $Tabl_Mendeleeva/mendeleeva/TablArea
 @export var atom_scene: PackedScene = preload("res://Scene/ATOM.tscn")
 @export var link: PackedScene = preload("res://Scene/LINK.tscn")
+# Ссылка на указку и контроллер
+@onready var raycast = $Pointer/RayCast3D
+@onready var pointer = $Pointer
+
 @export var link_length: float = 0.5  # Длина связи
 
 var atoms: Array = []  # Список всех атомов
 var links: Dictionary = {}  # Словарь существующих связей (ключ: "id1_id2")
+
+var is_aiming_at_table: bool = false	# флаг - наведена ли указка на таблицу
+var current_hit_point: Vector3 = Vector3.ZERO	# текущая точка попадания указки, по умолчанию ноль
 
 func _ready():
 	if not table_area:
@@ -17,11 +24,65 @@ func _ready():
 	table_area.area_entered.connect(_on_table_area_area_entered)
 	print("Сигнал таблицы подключен")
 	
+	# Подключаем сигналы от указки (XRToolsPickable)
+	if pointer:
+		# Сигнал когда указку взяли в руку
+		if pointer.has_signal("picked_up"):
+			pointer.picked_up.connect(_on_pointer_picked_up)
+		
+		# Сигнал когда указку отпустили
+		if pointer.has_signal("dropped"):
+			pointer.dropped.connect(_on_pointer_dropped)
+		
+		# Сигнал нажатия action кнопки (обычно это trigger или grip)
+		if pointer.has_signal("action_pressed"):
+			pointer.action_pressed.connect(_on_pointer_action_pressed)
+	
+	print("Система готова: возьмите указку и наведите на таблицу")
+	
 func _process(delta):
+	# Вызываем проверку луча каждый кадр
+	_handle_raycast()
 	# Проверяем все пары атомов каждый кадр
 	_update_all_links()
 	# Обновляем транформ всех связей
 	_update_existing_links_transforms()
+	
+func _handle_raycast():
+	if not raycast:
+		return
+	
+	if raycast.is_colliding():
+		var collider = raycast.get_collider()
+		
+		# Проверяем, попали ли в Area3D таблицы
+		if collider == table_area or collider.get_parent() == table_area:
+			is_aiming_at_table = true
+			current_hit_point = raycast.get_collision_point()
+			print("Указка наведена на таблицу")
+			return
+	
+	is_aiming_at_table = false
+	
+func _on_pointer_picked_up(_pickable):
+	print("Указка взята в руку")
+	# Включаем луч, если он был выключен
+	if raycast:
+		raycast.enabled = true
+
+func _on_pointer_dropped(_pickable):
+	print("Указка отпущена")
+	is_aiming_at_table = false
+	# Выключаем луч, чтобы не тратить ресурсы
+	if raycast:
+		raycast.enabled = false
+
+func _on_pointer_action_pressed(_pickable):
+	# Это вызывается, когда игрок нажимает action кнопку (обычно trigger)
+	# пока указка в руке
+	print("Нажат триггер")
+	if is_aiming_at_table:
+		_spawn_atom()	
 	
 func _on_table_area_area_entered(area: Area3D) -> void:
 	# Проверка, что это рука
